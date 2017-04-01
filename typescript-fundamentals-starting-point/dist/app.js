9e8a6f0290f50247d90f4667a714f46cc2d9a7e6
Sunny Mistry <sunnymis@buffalo.edu>
2017-03-31 22:51:42 -0400

    Initial commit

---
+++ typescript-fundamentals-starting-point/dist/app.js
@@ -0,0 +1,11 @@
+var symbol = '#';
+var length = 7;
+function generateRandomId() {
+    return symbol + Math.random().toString(36).substr(2, length);
+}
+function main() {
+    var appComponent = document.getElementById('app');
+    setInterval(function () {
+        appComponent.innerHTML = generateRandomId();
+    }, 1000);
+}
