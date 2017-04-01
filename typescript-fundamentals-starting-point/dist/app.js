14b7ef82549203f1aea0c6af939f41b5369ea6f7
Sunny Mistry <sunnymis@buffalo.edu>
2017-03-31 23:16:16 -0400

    types

--- typescript-fundamentals-starting-point/dist/app.js
+++ typescript-fundamentals-starting-point/dist/app.js
@@ -1,11 +1,12 @@
-var symbol = '#';
-var length = 7;
-function generateRandomId() {
+function generateRandomId(symbol, length) {
     return symbol + Math.random().toString(36).substr(2, length);
 }
 function main() {
     var appComponent = document.getElementById('app');
     setInterval(function () {
-        appComponent.innerHTML = generateRandomId();
+        appComponent.innerHTML = generateRandomId('#', 7);
     }, 1000);
+}
+function something() {
+    throw new Error('error');
 }
--- typescript-fundamentals-starting-point/src/app.ts
+++ typescript-fundamentals-starting-point/src/app.ts
@@ -1,13 +1,16 @@
-const symbol: string = '#';
-var length: number  = 7;
+type ValidSymbol = '#' | '$'
 
-function generateRandomId() {
+function generateRandomId(symbol: ValidSymbol, length: number): string {
     return symbol + Math.random().toString(36).substr(2, length)
 }
 
 function main() {
     var appComponent = document.getElementById('app')
     setInterval(function() {
-        appComponent.innerHTML = generateRandomId()
+        appComponent.innerHTML = generateRandomId('#', 7);
     }, 1000)
 } 
+
+function something(): never {
+	throw new Error('error');
+}
